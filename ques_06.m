20da5b5ce6a03e6b9f396fa751c934dd5158845e
alamgirakash2000 <alamgirakash2000@gmail.com>
2023-04-07 00:33:23 +0600

    with the last question

--- ques_06.m
+++ ques_06.m
@@ -1,36 +1,57 @@
 clc; clear; close all;
 
-% Define the range of x values
-x = -5:0.01:5;
+%% Part (a)
+x = linspace(-5, 5, 10000);
+y1 = sqrt(42 - x.^2);   % Equation 01
 
-% Calculate y values for Equation 1
-y1 = sqrt(42 - x.^2);
+% By rearranging equation 2 --> y = (2*y^2 - 6)/3 - x^4/3
+f = @(x) (2/3)*sqrt(3 + x.^4);
 
-% Calculate y values for Equation 2
-y2 = (2*y1.^2 - 6)./ (3 - x.^4);
+% Plotting the two equations
+fplot(f, [-5, 5])
+hold on
+plot(x, y1), ylim([-1,10])
+xlabel('x'), ylabel('y'), title('Plots: y vs x')
+legend('y = sqrt(42 -x^2)', 'y = (2*y^2 - 6)/3 + x^4/3')
 
-% Plot the two curves
-plot(x, y1, 'b-', x, y2, 'r--');
-xlabel('x');
-ylabel('y');
-legend('Equation 1', 'Equation 2');
-
-% Find the intersection points using fzero
-x_intercept = zeros(2, 1);
-y_intercept = zeros(2, 1);
-count = 0;
+fprintf("\n\n Result for part (a): \n");
 for i = 1:length(x)-1
-    f = @(y) x(i)^2 + y^2 - 42 - (x(i)^4 + 3*y - 2*y^2 - 6);
-    if sign(f(y1(i))) ~= sign(f(y1(i+1)))
-        [y_intercept_candidate, ~, exitflag] = fzero(f, [y1(i), y1(i+1)]);
-        if exitflag > 0 && isreal(y_intercept_candidate)
-            count = count + 1;
-            x_intercept(count) = x(i);
-            y_intercept(count) = y_intercept_candidate;
-        end
+    ym = x(i)^4 + 3*y1(i) - 2* y1(i)^2 - 6;
+    
+    if (ym>-0.05 && ym<0.05)
+        fprintf("x: %.2f, y: %.2f \n ", x(i), y1(i));
     end
 end
 
-% Print the intersection points
-fprintf('The intersection points are:\n');
-disp([x_intercept, y_intercept]);
+%% Part (b)
+fprintf("\n\n Result for part (b): \n");
+
+syms x y
+f = x^2 + y^2-42;
+disp("differentiation of eqn (i) with respect to x:");
+disp(diff(f, x));
+disp("Integration of eqn (i) with respect to x:");
+disp(int(f, x));
+
+syms x y
+f = x^2 + y^2-42;
+disp("differentiation of eqn (i) with respect to y:");
+disp(diff(f, y));
+disp("Integration of eqn (i) with respect to y:");
+disp(int(f, y));
+
+syms x y
+f = x^4 + 3*y-2*y^2-6;
+disp("differentiation of eqn (ii) with respect to x:");
+disp(diff(f, x));
+disp("Integration of eqn (ii) with respect to x:");
+disp(int(f, x));
+
+syms x y
+f = x^4 + 3*y-2*y^2-6;
+disp("differentiation of eqn (ii) with respect to y:");
+disp(diff(f, y));
+disp("Integration of eqn (ii) with respect to x:");
+disp(int(f, y));
+
+
